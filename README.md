e9b36d6e57ab6be894d242173cc698a0119fac77
RoniJackVituli <rjvtechnology@gmail.com>
2022-04-10 13:41:45 +0300

    change the code add new Data base

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 this project its my first project web that i created in django framework
 
+
+
+
+
+
+<img src="https://media.giphy.com/media/x9NDrPB70zTVTcCXR8/giphy.gif" />
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
-from django.db import models
 
+from django.db import models
+from datetime import date
+from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length=30)
+    date = models.DateField(auto_now=True, auto_now_add=False)
+    title = models.CharField(max_length=30)
+    image = models.CharField(max_length=50)
+    slug = models.SlugField(default="",null=False)
+    excerpt = models.CharField(max_length=50)
+    content = models.CharField(max_length=100)
+  
+    def save(self,*args,**kwargs): 
+      self.slug = slugify(self.title)
+      super().save(args,kwargs)
+
+    def get_correct_slug(self):
+      return reverse("dataPost",args=[self.slug])
+
+    def __str__(self):
+      return "author = {} and  title = {}".format(self.author,self.title)
